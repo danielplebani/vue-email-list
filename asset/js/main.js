643cee4a0ba30bb1e8ef9f8b4e00319a47c6f636
Daniel Plebani <135621639+danielplebani@users.noreply.github.com>
2024-01-12 20:07:24 +0100

    try a do a new commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,29 +10,22 @@ createApp({
   },
 
   methods:{
-    repeatEmail(){
-      for (let i = 0; i < 10; i++) {
-        axios 
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-          console.log(response.data.response)
-          this.randomEmail.push(response.data.response)
-
-          this.printTogether++
-        })
-        .catch(error => {
-          console.log('errore!!! fai attenzione', error);
-        })
-      }  
-    },
-
     clickButton(){
       this.buttonClicked = true
     }
-      
   },
 
   mounted(){
-    this.repeatEmail()
+    for (let i = 0; i < 10; i++) {
+      axios 
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(response => {
+        console.log(response.data.response)
+        this.randomEmail.push(response.data.response)
+      })
+      .catch(error => {
+        console.log('errore!!! fai attenzione', error);
+      })
+    }
   }
 }).mount('#app')
